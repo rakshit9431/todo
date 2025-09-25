009e19e1b2d475856f3f0919fe12f4e365d85a12
rakshit9431 <kumarrakshit9431@gmail.com>
2025-09-25 19:04:28 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,9 +3,8 @@ import Hero from "./Hero";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gray-100 p-4">
+    <div>
       <Hero />
-      {/* <Bheem /> */}
     </div>
   );
 }
